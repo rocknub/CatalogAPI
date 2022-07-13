2451880486bfc4175f00fb0d1e000783cd81f3e7
Brian Mendes <brian.mendes.f@gmail.com>
2022-07-13 15:21:13 -0300

    Atualização com Get funcional.

--- CatalogAPI/Controllers/ProdutosController.cs
+++ CatalogAPI/Controllers/ProdutosController.cs
@@ -1,8 +1,7 @@
-﻿using CatalogAPI.Models;
-using CatalogAPI.Context;
+﻿using CatalogAPI.Context;
+using CatalogAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace CatalogAPI.Controllers
 {
@@ -10,9 +9,9 @@ namespace CatalogAPI.Controllers
     [ApiController]
     public class ProdutosController : ControllerBase
     {
-        private readonly CatalogDbContext _context;
-        
-        public ProdutosController(CatalogDbContext context)
+        private readonly CatalogDbContext? _context;
+
+        public ProdutosController(CatalogDbContext? context)
         {
             _context = context;
         }
@@ -21,70 +20,22 @@ namespace CatalogAPI.Controllers
         public ActionResult<IEnumerable<Produto>> Get()
         {
             var produtos = _context.Produtos.ToList();
-
             if (produtos is null)
             {
-                return NotFound("Produto não encontrado.");
+                return NotFound("Produtos não encontrado.");
             }
-            
             return produtos;
         }
 
-        [HttpGet("{id:int}", Name="ObterProduto")]
+        [HttpGet("{id:int}")]
         public ActionResult<Produto> Get(int id)
         {
             var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
-
             if (produto is null)
             {
-                return NotFound("Produto não encontrado.");
+                return NotFound("Produto não encontrado. Tente novamente.");
             }
-
             return produto;
-        }
-
-        [HttpPost]
-        public ActionResult Post(Produto produto)
-        {
-            if (produto is null)
-                return BadRequest("Produto não encontrado.");
-
-            _context.Produtos.Add(produto);
-            _context.SaveChanges();
-
-            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
-        }
-
-        [HttpPut("{id:int}")]
-        public ActionResult Put(int id, Produto produto)
-        {
-            if (id != produto.ProdutoId)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry<Produto>(produto).State = EntityState.Modified;
-            _context.SaveChanges();
-
-            return Ok(produto);
-        }
-
-        [HttpDelete("{id:int}")]
-        public ActionResult Delete(int id)
-        {
-            var produto = _context.Produtos.FirstOrDefault(
-                p => p.ProdutoId == id);
-            // var produto = _context.Produtos.Find(id); Segunda opção caso metodo principal não exista;
-
-            if (produto is null)
-            {
-                return NotFound("Produto não encontrado.");
-            }
-
-            _context.Produtos.Remove(produto);
-            _context.SaveChanges();
-
-            return Ok(produto);
         }
     }
 }
