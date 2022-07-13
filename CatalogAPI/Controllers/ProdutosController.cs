9d4c6ba1a638880721cec6dd4312a7faf35f469b
Brian Mendes <brian.mendes.f@gmail.com>
2022-07-13 06:18:54 -0300

    Alterações iniciais para o método Get.

---
+++ CatalogAPI/Controllers/ProdutosController.cs
@@ -0,0 +1,45 @@
+﻿using CatalogAPI.Models;
+using CatalogAPI.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly CatalogDbContext _context;
+        
+        public ProdutosController(CatalogDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _context.Produtos.ToList();
+
+            if (produtos is null)
+            {
+                return NotFound("Produto não encontrado.");
+            }
+            
+            return produtos;
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+            if (produto is null)
+            {
+                return NotFound("Produto não encontrado.");
+            }
+
+            return produto;
+        }
+    }
+}
